0faf6d13580c6c6d24e478f450e6a4ffe17ab2a6
frankwang <frankwang95174@gmail.com>
2023-03-02 01:12:11 +0800

    Updates

---
+++ assets/IndexView-29e4bace.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-718f8dd3.js";const n={},o={class:"text-center"};function s(r,_){return c(),t("h1",o,"這是首頁")}const f=e(n,[["render",s]]);export{f as default};
--- assets/IndexView-e3ac4407.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as t}from"./index-f86386bb.js";const n={},o={class:"text-center"};function s(r,_){return c(),t("h1",o,"這是首頁")}const f=e(n,[["render",s]]);export{f as default};
